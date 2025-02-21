9f9bf5516ee40b80707ee716aa7ae0ee874772fd
DaePPang <disa13000@naver.com>
2025-02-21 15:04:19 +0900

    Loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+
+            Console.WriteLine("■□□□□□□");
+           Thread.Sleep(1000);                                 // 1초
+            Console.Clear();
+            Console.WriteLine("■■□□□□□");
+            Thread.Sleep(1000);                                 // 1초
+            Console.Clear();
+            Console.WriteLine("■■■□□□□");
+            Thread.Sleep(1000);                                 // 1초
+            Console.Clear();
+            Console.WriteLine("■■■■□□□");
+            Thread.Sleep(1000);                                 // 1초
+            Console.Clear();
+            Console.WriteLine("■■■■■□□");
+            Thread.Sleep(1000);                                 // 1초
+            Console.Clear();
+            Console.WriteLine("■■■■■■□");
+            Thread.Sleep(1000);                                 // 1초
+            Console.Clear();
+            Console.WriteLine("■■■■■■■");
+            Console.Clear();
+            Console.WriteLine(" Complete ");
+        }
+    }
+}
