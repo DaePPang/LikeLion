b282bd87a135e6d0d79e91d432efc518cf41bc57
DaePPang <disa13000@naver.com>
2025-02-21 16:02:14 +0900

    studyReadLine

---
+++ study6/study6/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. 사용자 입력을 문자열로 받기
+            //Console.Write("이름을 입력하세요 : ");
+            //string userName = Console.ReadLine();
+
+            //Console.WriteLine($"안녕하세요, {userName}님!");  //입력값 출력
+
+            //2.문자열을 정수로 변환
+            //Console.Write("나이를 입력하세요 : ");
+            //string input = Console.ReadLine();
+            //int age = int.Parse(input);
+
+            //Console.WriteLine($"내년에는 {age + 1}살이 되겠군요!");
+            //Console.WriteLine("내년에는 " + age + "살이 되겠군요!");
+            //Console.WriteLine("내년에는 {0} 살이 되겠군요!", age +1);
+
+/*
+            Console.Write("루인 스킬 피해 입력 값을 입력하세요 : ");
+            string stat1 = Console.ReadLine();
+            float nStat1 = float.Parse(stat1);
+            Console.Write("카드 게이지 획득량의 입력 값을 입력하세요 : ");
+            string stat2 = Console.ReadLine();
+            float nStat2 = float.Parse(stat2);
+            Console.Write("각성시 피해 입력 값을 입력하세요 : ");
+            string stat3 = Console.ReadLine();
+            float nStat3 = float.Parse(stat3);
+            Console.Write("최대 마나 입력 값을 입력하세요 : ");
+            string stat4 = Console.ReadLine();
+            int nStat4 = int.Parse(stat4);
+            Console.Write("전투 중 마나 회복량을 입력하세요 : ");
+            string stat5 = Console.ReadLine();
+            int nStat5 = int.Parse(stat5);
+            Console.Write("비전투 중 마나 회복량을  입력하세요 : ");
+            string stat6 = Console.ReadLine();
+            int nStat6 = int.Parse(stat6);
+            Console.Write("이동속도를 입력하세요 : ");
+            string stat7 = Console.ReadLine();
+            float nStat7 = float.Parse(stat7);
+            Console.Write("탈 것 속도를 입력하세요 : ");
+            string stat8 = Console.ReadLine();
+            float nStat8 = float.Parse(stat8);
+            Console.Write("운반 속도를 입력하세요 : ");
+            string stat9 = Console.ReadLine();
+            float nStat9 = float.Parse(stat9);
+            Console.Write("스킬 재사용 대기시간 감소 입력값을 입력하세요 : ");
+            string stat10 = Console.ReadLine();
+            float nStat10 = float.Parse(stat10);
+
+            Console.WriteLine("\n" + "=====" + "\n");
+
+            Console.WriteLine("루인 스킬 피해 : "+ nStat1+"%");
+            Console.WriteLine("카드 게이지 획득량 : "+ nStat2+"%");
+            Console.WriteLine("각성기 피해 : "+ nStat3+"%");
+            Console.WriteLine("최대 마나 : "+ nStat4);
+            Console.WriteLine("전투 중 마나 회복량 : "+ nStat5);
+            Console.WriteLine("비전투 중 마나 회복량 : "+ nStat6);
+            Console.WriteLine("이동 속도 : " + nStat7 + "%");
+            Console.WriteLine("탈 것 속도 : " + nStat8 + "%");
+            Console.WriteLine("운반 속도 : " + nStat9 + "%");
+            Console.WriteLine("스킬 재사용 대기시간 감소 : "+ nStat10 +"%");
+*/
+
+
+        }
+    }
+}
