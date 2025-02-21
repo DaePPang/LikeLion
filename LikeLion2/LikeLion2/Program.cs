c0c890c824785a316c55e0740c21b109ac0777d7
DaePPang <disa13000@naver.com>
2025-02-21 10:23:03 +0900

    Study2

---
+++ LikeLion2/LikeLion2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언
+            int age;                                                 // 정수형 변수
+            age = 10;                                              // 변수에 값을 저장함
+
+            Console.WriteLine(age);             //변수에 저장된 값을 출력         25
+
+            /*
+             * WriteLine를 사용하면 해당 값 출력 후 Enter(줄 바뀜)
+             * Write를 사용하면 해당 값 출력 후 줄이 바뀌지않음 !!!
+             */
+        }
+    }
+}
